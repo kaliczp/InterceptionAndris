bfed9e4232336c771c4ed0131e6532117d2f1561
Kalicz Péter <kaliczp@gmail.com>
2025-02-03 12:30:17 +0100

    Calculate monthly sums and save
    The colSum() funciton is your friend.

--- HaviSum.R
+++ HaviSum.R
@@ -3,3 +3,7 @@ raw <- read_excel("PREC and INT.xlsx")
 library(xts)
 Sys.setenv(TZ = "UTC")
 precint.xts <- xts(as.matrix(raw[,2:3]), raw[,1,drop = TRUE])
+
+Monthlyprecint <- apply.monthly(precint.xts, colSums)
+
+write.zoo(Monthlyprecint, file = "Monthly.csv")
