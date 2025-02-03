e76b779ccf35dafd37c356ec8276415096c27f92
Kalicz Péter <kaliczp@gmail.com>
2025-02-03 12:34:01 +0100

    Round to 2 decimal

--- HaviSum.R
+++ HaviSum.R
@@ -4,6 +4,6 @@ library(xts)
 Sys.setenv(TZ = "UTC")
 precint.xts <- xts(as.matrix(raw[,2:3]), raw[,1,drop = TRUE])
 
-Monthlyprecint <- apply.monthly(precint.xts, colSums)
+Monthlyprecint <- apply.monthly(precint.xts, function(x){round(colSums(x),2)})
 
 write.zoo(Monthlyprecint, file = "Monthly.csv")
